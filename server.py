1b5d73d2077889166fca1156bbb7bee2044fd4a9
Sanket Singla <mrockdud28@gmail.com>
2021-10-19 11:13:42 +0530

    image check added

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
-import flask
 from flask import Flask, flash, render_template, redirect, request, send_file, url_for
+import random
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisistheserverkey'
@@ -24,6 +25,16 @@ def home():
         if image_file.filename == '':
             flash('No file was uploaded.')
             return redirect(request.url)
+
+        if image_file and is_allowed_file(image_file.filename):
+            passed = False
+            try:
+                filename = generate_random_name(image_file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                image_file.save(filepath)
+                passed = True
+            except Exception:
+                passed = False
 
         # if the file is "legit"
         if image_file:
@@ -64,6 +75,18 @@ def is_allowed_file(filename):
     allowed_ext = filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
     return '.' in filename and allowed_ext
 
+LETTER_SET = list(set('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'))
+
+def generate_random_name(filename):
+    """ Generate a random name for an uploaded file. """
+    ext = filename.split('.')[-1]
+    rns = [random.randint(0, len(LETTER_SET) - 1) for _ in range(3)]
+    chars = ''.join([LETTER_SET[rn] for rn in rns])
+
+    new_name = "{new_fn}.{ext}".format(new_fn=chars, ext=ext)
+    new_name = secure_filename(new_name)
+
+    return new_name
 
 if __name__ == "__main__":
     app.run('127.0.0.1')
